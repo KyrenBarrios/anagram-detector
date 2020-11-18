ef3252ba8d54e6a305450d1b574b44a045b2d93a
Kyren Barrios <kyrenbarrios@gmail.com>
2020-11-17 16:51:27 -0800

    commit tree 355f8e234fea5b124042c8679bb13ec46403a895

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,21 @@
-# Your code goes here!
+class Anagram
+    attr_accessor :word
+
+    def initialize (word)
+      @word = word
+      @letters = @word.split("").sort
+    end 
+
+    def match(possinble_anagram)
+        possinble_anagram.select do |possinble_anagram|
+            @letters == possinble_anagram.split("").sort
+        end
+    end 
+
+
+
+
+
+
+
+end 
